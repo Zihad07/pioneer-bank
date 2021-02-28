5a2cbb29054462d276d43553f2f8ae3b5acb1929
Zihad <zihadnayem8@gmail.com>
2021-02-28 16:25:59 +0600

    added deposite and current balance operation.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 console.log('connect');
 
-var loginBtn = document.getElementById('login');
-var user = document.getElementById('name');
-var password = document.getElementById('password');
-var loginSection = document.getElementById('login-area');
-document.get
+const loginBtn = document.getElementById('login');
+const user = document.getElementById('name');
+const password = document.getElementById('password');
+const loginSection = document.getElementById('login-area');
 
+
+// Transaction section variable
+const transactionArea = document.getElementById("transaction-area");
+const depositBtn = document.getElementById("depositBtn");
+const withdrawBtn = document.getElementById("withdrawBtn");
+const depositAmount = document.getElementById("depositAmount");
+const withdrawAmount = document.getElementById('withdrawAmount');
+const currentDeposit = document.getElementById("currentDeposit");
+const currentBalance = document.getElementById("currentBalance");
+const currentWithdraw = document.getElementById("currentWithdraw");
+
+// loginBtn handler
 loginBtn.addEventListener('click', function(e) {
     console.log('login');
     console.log(e.target.value);
@@ -13,4 +24,33 @@ loginBtn.addEventListener('click', function(e) {
     // console.log(loginSection.innerHTML)
     // loginSection.remove();
     loginSection.style.display = 'none';
+    transactionArea.style.display = 'block';
+});
+
+// depositeBtn handler
+
+function updateSpantext(value, obj) {
+    let current_balance = parseFloat(obj.innerText);
+    current_balance += value;
+    obj.innerText = current_balance;
+}
+
+depositBtn.addEventListener('click', function(e) {
+
+    let value = parseFloat(depositAmount.value);
+    if (!isNaN(value)) {
+        updateSpantext(value, currentDeposit);
+        updateSpantext(value, currentBalance);
+        depositAmount.value = '';
+    }
+});
+
+withdrawBtn.addEventListener('click', function(e) {
+    let value = parseFloat(withdrawAmount.value);
+    // alert(currentWithdraw.innerText);
+
+    if (!isNaN(value)) {
+        updateSpantext(value, currentWithdraw);
+        withdrawAmount.value = '';
+    }
 });
